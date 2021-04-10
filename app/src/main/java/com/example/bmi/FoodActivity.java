654040740c946c43e68e7499d682182493cc6422
Norbert Daniluk <norbert.daniluk@gmail.com>
2021-04-10 19:00:27 +0200

    added dishes

--- app/src/main/java/com/example/bmi/FoodActivity.java
+++ app/src/main/java/com/example/bmi/FoodActivity.java
@@ -26,16 +26,22 @@ public class FoodActivity extends AppCompatActivity {
         float height = Float.parseFloat(myIntent.getStringExtra("height"));
         String sex = myIntent.getStringExtra("sex");
         int age = Integer.parseInt(myIntent.getStringExtra("age"));
+        TextView dishOutput = findViewById(R.id.dish);
 
         String resultText;
+        String dishText = "";
         float bmi = weight / ((height / 100) * (height / 100));
         if (bmi > 0 && bmi <= 18.5) {
+            dishText = "2 egg brown bread sandwich + green chutney + 1 cup milk + 3 cashews + 4 almonds + 2 walnuts";
             resultText = EBMIResult.You_are_underweight.toString();
         } else if (bmi > 18.5 && bmi <= 25) {
+            dishText = "Spaghetti Bolognese";
             resultText = EBMIResult.Your_weight_is_fine.toString();
         } else if (bmi > 25 && bmi <= 30) {
+            dishText = "Cranberry Asparagus with Pine Nuts";
             resultText = EBMIResult.You_are_overweight.toString();
         } else {
+            dishText = "Mediterranean Pasta Salad";
             resultText = EBMIResult.You_are_obese.toString();
         }
 
@@ -50,7 +56,7 @@ public class FoodActivity extends AppCompatActivity {
         TextView kcalOutput = findViewById(R.id.kcal);
         resultOutput.setText(resultText);
         kcalOutput.setText(String.format("You should eat %s kcal a day", kcal));
-        TextView dishOutput = findViewById(R.id.dish);
-        dishOutput.setText("No matter what is your BMI, a salad is always a good option :)");
+
+        dishOutput.setText(String.format("Dish for you - %s", dishText));
     }
 }
